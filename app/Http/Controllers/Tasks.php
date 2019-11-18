7920c6dab6ef42a24e4c28d0093fb2c20a24beb4
ChrisMeliniotou <chris.melinioti@gmail.com>
2019-11-18 07:47:42 +0100

    create action on controller and view

--- app/Http/Controllers/Tasks.php
+++ app/Http/Controllers/Tasks.php
@@ -2,12 +2,54 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+
 use DB;
 use App\Task;
 
-use Illuminate\Http\Request;
+/**
+ * * @return \Illuminate\Http\Response
+*/
 
-class Tasks extends Controller
-{
-    //
+class Tasks extends Controller {
+public function index() {
+    
 }
+public function store(Request $request)
+{
+    $request->validate([
+      'title'=>'required',
+      'details'=>'required',
+    ]);
+
+    $contact = new Task([
+        'title'=> $request->get('title'),
+        'details'=> $request->get('details')
+    ]);
+   $contact->save();
+   return redirect('/tasks')->with('success', 'Task saved!');
+}
+
+function create() 
+{
+    return view('tasks.create');
+}
+
+/**
+ * @param \Illuminate\Http\Request $_request
+ * @return \Illuminate\Http\Response
+ *  */ 
+
+/**
+ * Display the specified resource
+ * 
+ * @param int $id
+ * @reeturn \Illuminate\Http\Response
+ */
+
+ function show($id)
+ {
+     //
+ }
+
+} 
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,5 @@
 Route::get('/', function () {
     return View('content');
 });
+
+Route::resource('tasks', 'Tasks');
