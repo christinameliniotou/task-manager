20e7bf89f0bbd326b70cea3701244f37dd267744
Christina Meliniotou <chris.melinioti@gmail.com>
2019-12-18 08:00:54 +0100

    Update Tasks.php

--- app/Http/Controllers/Tasks.php
+++ app/Http/Controllers/Tasks.php
@@ -12,69 +12,72 @@ use App\Task;
 */
 
 class Tasks extends Controller {
-public function index() {
-    $tasks = Task::all();
 
-    return view("tasks.index", compact('tasks'));
-}
-public function store(Request $request)
-{
-    $request->validate([
-      'title'=>'required',
-      'details'=>'required',
-    ]);
+    public function index() {
+        $tasks = Task::all();
 
-    $contact = new Task([
-        'title'=> $request->get('title'),
-        'details'=> $request->get('details')
-    ]);
-   $contact->save();
-   return redirect('/tasks')->with('success', 'Task saved!');
-}
+        return view("tasks.index", compact('tasks'));
+    }
 
-function create() 
-{
-    return view('tasks.create');
-}
-
-function edit($id)
-{
-    $task = Task::find($id);
-    return view('tasks.edit', compact('task'));
-}
-
-public function update(Request $request, $id)
-{
-    $request->validate([
+    public function store(Request $request)
+    {
+        $request->validate([
         'title'=>'required',
-        'details'=>'required'
-    ]);
+        'details'=>'required',
+        ]);
 
-    $task = Task::find($id);
-    $task->title = $request->get('title');
-    $task->details = $request->get('details');
-    $task->save();
-    
-    return redirect('/tasks')->with('success', 'Task updated');
-}
-/**
- * @param \Illuminate\Http\Request $_request
- * @return \Illuminate\Http\Response
- *  */ 
+        $contact = new Task([
+            'title'=> $request->get('title'),
+            'details'=> $request->get('details')
+        ]);
+    $contact->save();
+    return redirect('/tasks')->with('success', 'Task saved!');
+    }
 
-/**
- * Display the specified resource
- * 
- * @param int $id
- * @reeturn \Illuminate\Http\Response
- */
+    function create() 
+    {
+        return view('tasks.create');
+    }
 
-public function destroy($id)
-{
-    $task = Task::find($id);
-    $task->delete();
+    function edit($id)
+    {
+        $task = Task::find($id);
+        return view('tasks.edit', compact('task'));
+    }
 
-    return redirect('/tasks')->with('success', 'Contact deleted!');
-}
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'title'=>'required',
+            'details'=>'required'
+        ]);
+
+        $task = Task::find($id);
+        $task->title = $request->get('title');
+        $task->details = $request->get('details');
+        $task->save();
+        
+        return redirect('/tasks')->with('success', 'Task updated');
+    }
+
+    /**
+     * @param \Illuminate\Http\Request $_request
+     * @return \Illuminate\Http\Response
+     *  */ 
+
+    /**
+     * Display the specified resource
+     * 
+     * @param int $id
+     * @reeturn \Illuminate\Http\Response
+     */
+
+    public function destroy($id)
+    {
+        $task = Task::find($id);
+        $task->delete();
+
+        return redirect('/tasks')->with('success', 'Contact deleted!');
+    }
 
 } 
